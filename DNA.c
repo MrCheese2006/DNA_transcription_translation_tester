51f99318accc15251ec9fc4399d0135477ec560f
MrCheese2006 <oak.a.k2006@gmail.com>
2026-02-11 22:32:59 -0500

    Implemented DNA code and template generator, and mRNA answer generators

--- DNA.c
+++ DNA.c
@@ -1,21 +1,187 @@
 # include "stdio.h"
 # include "stdlib.h"
 # include "string.h"
+# include "time.h"
 
-char ** DNA_code_generator() {
-    // a function that generates a random gene from the DNA code strand
+// could implement a "streak tracker"
+
+char *DNA_code_generator() {
+    // a function that generates a random gene from DNA
+
+    int multiplier = (rand() % 6) + 3; // have 3-8 codons in the string
+    int size = 4 * multiplier - 1; // (3 bases + space) * number _of_codons - no_space_at_end = size of string
+    char *code = (char *)malloc(size+1);
+    *(code + size) = '\0';
+
+    // start codon
+    *(code + 0) = 'A';
+    *(code + 1) = 'T';
+    *(code + 2) = 'G';
+    
+
+    int len = strlen(code);
+
+    int mode;
+    int i = 3;
+
+    for (i; i <= len - 3; i++) {
+        
+        if ((i+1) % 4 ==  0 & i != 0) { // break the sequence into codons
+            *(code + i) = ' ';
+            continue;
+        }
+
+        mode = rand() % 4; // get random numbers from 0 to 3
+
+        if (mode == 0) { // select base from random number
+            *(code + i) = 'A';
+        }
+        else if (mode == 1) {
+            *(code + i) = 'T';
+        } 
+        else if (mode == 2) {
+            *(code + i) = 'G';
+        }    
+        else {
+            *(code + i) = 'C';
+        }
+    }
+
+    // stop codon in DNA code
+    int stop_mode = rand() % 3; // random numer to choose stop codon
+    if (stop_mode == 0) {
+        *(code + len-3) = 'T';
+        *(code + len-2) = 'A';
+        *(code + len-1) = 'A';
+    }
+    else if (stop_mode == 1) {
+        *(code + len-3) = 'T';
+        *(code + len-2) = 'A';
+        *(code + len-1) = 'G';
+    }
+    else {
+        *(code + len-3) = 'T';
+        *(code + len-2) = 'G'; 
+        *(code + len-1) = 'A'; 
+    }
+
+    return code;
+
 }
 
-char ** DNA_template_generator() {
-    // a function that generates a random gene from the DNA template strand
+char * DNA_template_generator() {
+    // a function that generates a random gene template from DNA
+
+    int multiplier = (rand() % 6) + 3; // have 2-7 codons in the string
+    int size = 4 * multiplier - 1; // (3 bases + space) * number _of_codons - no_space_at_end = size of string
+    char *template = (char *)malloc(size+1);
+    *(template + size) = '\0';
+
+    // start codon
+    *(template + 0) = 'T';
+    *(template + 1) = 'A';
+    *(template + 2) = 'C';
+    
+
+    int len = strlen(template);
+
+    int mode;
+    int i = 3;
+
+    for (i; i <= len - 3; i++) {
+        
+        if ((i+1) % 4 ==  0 & i != 0) { // break the sequence into codons
+            *(template + i) = ' ';
+            continue;
+        }
+
+        mode = rand() % 4; // get random numbers from 0 to 3
+
+        if (mode == 0) { // select base from random number
+            *(template + i) = 'A';
+        }
+        else if (mode == 1) {
+            *(template + i) = 'T';
+        } 
+        else if (mode == 2) {
+            *(template + i) = 'G';
+        }    
+        else {
+            *(template + i) = 'C';
+        }
+    }
+
+    // stop codon in DNA template
+    int stop_mode = rand() % 3; // random numer to choose stop codon
+    if (stop_mode == 0) {
+        *(template + len-3) = 'A';
+        *(template + len-2) = 'T';
+        *(template + len-1) = 'T';
+    }
+    else if (stop_mode == 1) {
+        *(template + len-3) = 'A';
+        *(template + len-2) = 'T';
+        *(template + len-1) = 'C';
+    }
+    else {
+        *(template + len-3) = 'A';
+        *(template + len-2) = 'C'; 
+        *(template + len-1) = 'T'; 
+    }
+
+    return template;
 }
 
-char ** mRNA_from_code_ans() {
+char *mRNA_from_code_ans(char *code) {
     // generates the mRNA strand given a DNA code strand
+
+    int len = strlen(code); // length of coding strand, not including null character
+
+    char *mRNA = (char *)malloc(len+1); // initialize mRNA from code string
+    mRNA[len] = '\0';
+
+    for (int i = 0; i < len; i++) {
+
+        if (code[i] == 'T') {
+            mRNA[i] = 'U';
+        }
+        else {
+            mRNA[i] = code[i];
+        }
+    }
+
+    return mRNA;
 }
 
-char ** mRNA_from_template_ans() {
+char *mRNA_from_template_ans(char *template) {
     // generates the mRNA strand given a DNA template strand
+
+    int len = strlen(template); // length of coding strand, not including null character
+
+    char *mRNA = (char *)malloc(len+1); // initialize mRNA from code string
+    mRNA[len] = '\0';
+
+    for (int i = 0; i < len; i++) {
+
+        if (template[i] == 'A') {
+            mRNA[i] = 'U';
+        }
+        else if (template[i] == 'T') {
+            mRNA[i] = 'A';
+        }
+        else if (template[i] == 'G') {
+            mRNA[i] = 'C';
+        }
+        else if (template[i] == 'C') {
+            mRNA[i] = 'G';
+        }
+        else {
+            mRNA[i] = ' ';
+        }
+    }
+
+    return mRNA;
+
 }
 
 int compare_transcription() {
@@ -24,18 +190,15 @@ int compare_transcription() {
 
 int DNA_tester() {
 
-    int start = 1;
-    int cont;
+    int cont = 1;
 
-    while (start == 1 || cont == 1){
-
-        start = 0;
+    while (cont) {
 
         int mode = rand() % 3; // get either 0, 1 or 2 for 3 transcription from template, transcription from code and translation to AA's
 
-        if (mode == 0) { // transcription from template
+        if (mode == 0) { // transcription from DNA template strand
 
-            char * template = * DNA_template_generator(); // generate the template
+            char * template = DNA_template_generator(); // generate the template
 
             char *mRNA_attempt;
             printf("Enter the transcription of the DNA template: "); scanf("%s", mRNA_attempt); // get the users' attempt at mRNA transcription
@@ -43,31 +206,45 @@ int DNA_tester() {
             compare_transcription(); // compare the transcription
 
         }
-        else if (mode == 1) {  // transcription from code
-            
-            char * code = * DNA_code_generator(); // generate the code
+        else if (mode == 1) {  // transcription from DNA code strand
+
+            char * code = DNA_code_generator(); // generate the code
 
             char *mRNA_attempt;
             printf("Enter the transcription of the DNA code: "); scanf("%s", mRNA_attempt); // get the users' attempt at mRNA transcription
 
-            compare_transcription(); // compare the transcrpition
+            compare_transcription(); // compare the transcription
 
         }
         else { // translation to AA's
 
         }
 
-        printf("Would you like to continue (1/0)?: "); scanf("%d", cont); // check if they want to continue
+        printf("%s", "Would you like to continue (enter 1 or 0)? "); scanf("%d", cont); // check if they want to continue
+
+        if (!cont) {
+            printf("%s", "Session Terminated. Please run code again to start over.");
+        }
 
     }
-
-
 
     return 0;
 }
 
 int main() {
 
-    
+    srand(time(NULL)); // seed the code so rand() produces a new number each time
 
+    char * code = DNA_code_generator();
+    char * mRNA_from_code = mRNA_from_code_ans(code);
+    // char * template = DNA_template_generator();
+
+    printf("DNA code: %s\n    mRNA: %s\n", code, mRNA_from_code);
+
+    char * template = DNA_template_generator();
+    char * mRNA_from_template = mRNA_from_template_ans(template);
+
+    printf("DNA template: %s\n        mRNA: %s\n", template, mRNA_from_template);
+
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,5 +1,7 @@
-#include "stdio.h"
+# include "stdio.h"
 # include "stdlib.h"
+# include "string.h"
+# include "time.h"
 
 int main() {
     
@@ -9,11 +11,76 @@ int main() {
 
     // printf("Your number is %d", num);
 
-    int *str;
+    // int *str;
 
-    scanf("%s", str);
+    // scanf("%s", str);
 
-    printf("%s", str);
+    // printf("%s", str);
 
+    srand(time(NULL)); // seed the code so rand() produces a new number each time
+    int multiplier = (rand() % 5) + 2; // have 2-7 codons in the string
+    int size = 4 * multiplier - 1; // (3 bases + space) * number _of_codons - no_space_at_end = size of string
+    char *str2 = (char *)malloc(size+1);
+    *(str2 + size) = '\0';
+
+    // start codon
+    *(str2 + 0) = 'T';
+    *(str2 + 1) = 'A';
+    *(str2 + 2) = 'C';
+    
+
+    int len = strlen(str2);
+
+    printf("%d \n", len);
+
+    int mode;
+    int i = 3;
+
+    for (i; i <= len - 3; i++) {
+        
+        if ((i+1) % 4 ==  0 & i != 0) { // break the sequence into codons
+            *(str2 + i) = ' ';
+            continue;
+        }
+
+        mode = rand() % 4; // get random numbers from 0 to 3
+
+        printf("%d \n", mode);
+
+        if (mode == 0) { // select base from random number
+            *(str2 + i) = 'A';
+        }
+        else if (mode == 1) {
+            *(str2 + i) = 'T';
+        } 
+        else if (mode == 2) {
+            *(str2 + i) = 'G';
+        }    
+        else {
+            *(str2 + i) = 'C';
+        }
+
+    }
+
+    // stop codon in DNA template
+    int stop_mode = rand() % 3; // random numer to choose stop codon
+    if (stop_mode == 0) {
+        *(str2 + len-3) = 'A';
+        *(str2 + len-2) = 'T';
+        *(str2 + len-1) = 'T';
+    }
+    else if (stop_mode == 1) {
+        *(str2 + len-3) = 'A';
+        *(str2 + len-2) = 'T';
+        *(str2 + len-1) = 'C';
+    }
+    else {
+        *(str2 + len-3) = 'A';
+        *(str2 + len-2) = 'C'; 
+        *(str2 + len-1) = 'T'; 
+    }
+
+
+    printf("%s", str2);
     return 0;
 }
