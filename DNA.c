6db14d1ca87c67f75fe34c22f93ee58ef97860e9
MrCheese2006 <oak.a.k2006@gmail.com>
2026-02-18 13:21:30 -0500

    finished transcription for template and code, implemented translator for mRNA

--- DNA.c
+++ DNA.c
@@ -184,49 +184,252 @@ char *mRNA_from_template_ans(char *template) {
 
 }
 
-int compare_transcription() {
+int compare_transcription(char *mRNA, char *input) {
     // compares the generated mRNA strand answer with the user-inputted mRNA strand
+
+    int len_mRNA = strlen(mRNA);
+
+    for (int i = 0; i < len_mRNA; i++) {
+        if (mRNA[i] != input[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+
+}
+
+char AA_translator(char *codon) {
+
+    if (codon[0] == 'A') {
+        if (codon[1] == 'A') {
+            if (codon[2] ==  'A') {
+                return 'K';
+            }
+            else if (codon[2] ==  'G') {
+                return 'K';
+            }
+            else if (codon[2] ==  'U') {
+                return 'N';
+            }
+            else { // C
+                return 'N';
+            }
+        }
+        else if (codon[1] == 'G') {
+            if (codon[2] ==  'A') {
+                return 'R';
+            }
+            else if (codon[2] ==  'G') {
+                return 'R';
+            }
+            else if (codon[2] ==  'U') {
+                return 'S';
+            }
+            else { // C
+                return 'S';
+            }
+        }
+        else if (codon[1] == 'U') {
+            if (codon[2] ==  'A') {
+                return 'I';
+            }
+            else if (codon[2] ==  'G') {
+                return 'M';
+            }
+            else if (codon[2] ==  'U') {
+                return 'I';
+            }
+            else { // C
+                return 'I';
+            }            
+        }
+        else { // C
+            return 'T'; // AC_ all return C
+        }
+    }
+    else if (codon[0] == 'G') {
+        if (codon[1] == 'A') {
+            if (codon[2] ==  'A') {
+                return 'E';
+            }
+            else if (codon[2] ==  'G') {
+                return 'E';
+            }
+            else if (codon[2] ==  'U') {
+                return 'D';
+            }
+            else { // C
+                return 'D';
+            }
+        }
+        else if (codon[1] == 'G') {
+            return 'G'; // all GG_ return G        
+        }
+        else if (codon[1] == 'U') {
+            return 'V'; // all GU_ return V       
+        }
+        else { // C
+            return 'A'; // all GC_ return A     
+        }
+    }
+    else if (codon[0] == 'U') {
+        if (codon[1] == 'A') {
+            if (codon[2] ==  'A') {
+                return 's'; // s = stop
+            }
+            else if (codon[2] ==  'G') {
+                return 's'; // s = stop
+            }
+            else if (codon[2] ==  'U') {
+                return 'Y';
+            }
+            else { // C
+                return 'Y';
+            }
+        }
+        else if (codon[1] == 'G') {
+            if (codon[2] ==  'A') {
+                return 's'; // s = stop
+            }
+            else if (codon[2] ==  'G') {
+                return 'W';
+            }
+            else if (codon[2] ==  'U') {
+                return 'C';
+            }
+            else { // C
+                return 'C';
+            }            
+        }
+        else if (codon[1] == 'U') {
+            if (codon[2] ==  'A') {
+                return 'L';
+            }
+            else if (codon[2] ==  'G') {
+                return 'L';
+            }
+            else if (codon[2] ==  'U') {
+                return 'F';
+            }
+            else { // C
+                return 'F';
+            }            
+        }
+        else { // C
+            return 'S'; // all UC_ return S
+        }
+    }
+    else { // C
+        if (codon[1] == 'A') {
+            if (codon[2] ==  'A') {
+                return 'Q';
+            }
+            else if (codon[2] ==  'G') {
+                return 'Q';
+            }
+            else if (codon[2] ==  'U') {
+                return 'H';
+            }
+            else { // C
+                return 'H';                
+            }
+        }
+        else if (codon[1] == 'G') {
+            return 'R';
+        }
+        else if (codon[1] == 'U') {
+            return 'L'; // all CU_ return L
+        }
+        else { // C
+            return 'P'; // all CC_ return P
+        }
+    }
+
 }
 
 int DNA_tester() {
 
     int cont = 1;
 
+    int mode;
+    char *template;
+    char *code;
+    char *mRNA;
+    char *mRNA_attempt;
+    int len;
+    int res;
+    // char *cont_str;
+
     while (cont) {
 
-        int mode = rand() % 3; // get either 0, 1 or 2 for 3 transcription from template, transcription from code and translation to AA's
+        mode = rand() % 2; // get either 0, 1 or 2 for 3 transcription from template, transcription from code and translation to AA's
+        printf("%d\n", mode);
 
         if (mode == 0) { // transcription from DNA template strand
 
-            char * template = DNA_template_generator(); // generate the template
+            template = DNA_template_generator(); // generate the template
+            printf("Here is a DNA gene (template strand). Enter the transcription: %s\n", template);
+            mRNA = mRNA_from_template_ans(template); // generate the answer
+            len = strlen(mRNA);
+            mRNA_attempt = (char *)malloc(len + 1); // allocate space for input
 
-            char *mRNA_attempt;
-            printf("Enter the transcription of the DNA template: "); scanf("%s", mRNA_attempt); // get the users' attempt at mRNA transcription
+            printf("Enter the transcription of the DNA template:                   "); fgets(mRNA_attempt, len+1, stdin); printf("\n"); // get the users' attempt at mRNA transcription
 
-            compare_transcription(); // compare the transcription
+            res = compare_transcription(mRNA, mRNA_attempt); // compare the transcription
+
+            if (res) {
+                printf("Correct!\n");
+            }
+            else {
+            printf("Incorrect. The correct answer is                               %s\n", mRNA);
+            }
+
+            free(template);
+            free(mRNA);
+            free(mRNA_attempt);
 
         }
         else if (mode == 1) {  // transcription from DNA code strand
 
-            char * code = DNA_code_generator(); // generate the code
+            code = DNA_code_generator(); // generate the code
+            printf("Here is a DNA gene (code strand). Enter the transcription: %s\n", code);
+            mRNA = mRNA_from_code_ans(code); // generate the answer
+            len = strlen(mRNA);
+            mRNA_attempt = (char *)malloc(len + 1); // allocate space for input
 
-            char *mRNA_attempt;
-            printf("Enter the transcription of the DNA code: "); scanf("%s", mRNA_attempt); // get the users' attempt at mRNA transcription
+            printf("Enter the transcription of the DNA code:                   "); fgets(mRNA_attempt, len+1, stdin); printf("\n"); // get the users' attempt at mRNA transcription
 
-            compare_transcription(); // compare the transcription
+            res = compare_transcription(mRNA, mRNA_attempt); // compare the transcription
+
+            if (res) {
+                printf("Correct!\n");
+            }
+            else {
+                printf("Incorrect. The correct answer is                           %s\n", mRNA);
+            }
+
+            free(code);
+            free(mRNA);
+            free(mRNA_attempt);
 
         }
         else { // translation to AA's
 
         }
 
-        printf("%s", "Would you like to continue (enter 1 or 0)? "); scanf("%d", cont); // check if they want to continue
+        // cont_str = (char *)malloc(100);
 
-        if (!cont) {
-            printf("%s", "Session Terminated. Please run code again to start over.");
-        }
+        printf("Would you like to continue (enter 1 or 0)? "); 
+        scanf("%d", &cont);
+        getchar();
+        printf("\n"); // get the users' attempt at mRNA transcription // check if they want to continue
+
+        // free(cont_str);
 
     }
+
+    printf("Session Terminated. Please run code again to start over.");
 
     return 0;
 }
@@ -235,16 +438,7 @@ int main() {
 
     srand(time(NULL)); // seed the code so rand() produces a new number each time
 
-    char * code = DNA_code_generator();
-    char * mRNA_from_code = mRNA_from_code_ans(code);
-    // char * template = DNA_template_generator();
-
-    printf("DNA code: %s\n    mRNA: %s\n", code, mRNA_from_code);
-
-    char * template = DNA_template_generator();
-    char * mRNA_from_template = mRNA_from_template_ans(template);
-
-    printf("DNA template: %s\n        mRNA: %s\n", template, mRNA_from_template);
+    DNA_tester();
 
     return 0;
 }
